fa6bfd119dcebd4df42c89ec9c77808c600f1d86
Yousef Amar <yousefamar@gmail.com>
2019-04-07 13:21:14 +0000

    Upgrade dependencies and migrate to Webpack v3

--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,22 @@ var webpack = require('webpack');
 
 module.exports = {
 	entry: './src/index.ls',
+	mode: 'production',
 	output: {
 		path: __dirname + '/build',
 		filename: 'index.min.js'
 	},
 	module: {
-		loaders: [
-			{ test: /\.ls$/, loader: 'livescript' },
-			{ test: /\.jade$/, loader: "jade" },
-			{ test: /\.styl$/, loader: 'style-loader!css-loader!stylus-loader' },
-			{ test: /\.svg$/, loader: "url-loader?limit=0" }
+		rules: [
+			{ test: /\.ls$/, use: 'livescript-loader' },
+			{ test: /\.jade$/, use: 'jade-loader' },
+			{ test: /\.svg$/, use: 'url-loader?limit=0' },
+			{ test: /\.styl$/, use: [
+				'style-loader',
+				'css-loader',
+				'stylus-loader'
+			]}
 		]
 	},
-	debug: true,
-	devtool: 'source-map',
-	plugins: [
-		new webpack.optimize.UglifyJsPlugin({
-			compress: {
-				warnings: false
-			},
-			sourceMap: true,
-			mangle: false
-		})
-	]
+	devtool: 'source-map'
 };
